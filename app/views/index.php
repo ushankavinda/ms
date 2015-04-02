fc2fd17ebc3abd925eb04996eefb4f5482ddb786
ushankavinda <ushanrocky@gmail.com>
2015-04-03 01:23:05 +0530

    My Laravel Template Master

---
+++ app/views/index.php
@@ -0,0 +1,19 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+        
+        <!-- Angular-->
+        <script src="<?php echo asset('_/angular/angular.min.js'); ?>"></script>
+        <!--Bootstrap-->
+        <link type="text/css" rel="stylesheet" href="<?php echo asset('_/css/bootstrap.min.css'); ?>">
+    </head>
+    <body>
+        <h3>Welcome</h3>
+        <h4>My laravel template with Bootstrap, Angular</h4>
+        
+        <script src="<?php echo asset('_/js/jquery.min.js'); ?>"></script>
+        <script src="<?php echo asset('_/js/bootstrap.min.js'); ?>"></script>
+    </body>
+</html>
